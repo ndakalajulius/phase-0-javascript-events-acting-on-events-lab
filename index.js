f056d1e0d966452977c76e88117c0b79cf17026c
ndakalajulius <julius.ndakala@student.moringaschool>
2022-08-14 20:35:06 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
+var dodger = document.getElementById("dodger");
+undefined
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+undefined
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+var dodger = document.getElementById('dodger');
+      
+dodger.style.backgroundColor = "#ff69b4";
+      
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+        
+  if ( left > 0 ) {
+    dodger.style.left = `${left - 4}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if ( left < 360) {
+    dodger.style.left = `${left + 4}px`;
+  }
+}
+      
+document.addEventListener('keydown', function(e) {
+  if ( e === 37 ) {
+    moveDodgerLeft();
+  }
+  if ( e === 39 ) {
+    moveDodgerRight();
+  }
+});
+
 // Your code here
